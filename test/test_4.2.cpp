5c6df35649312e91c5784f2397ae9b73fc52be1d
jxxn92 <futjxxn@naver.com>
2023-04-22 17:49:04 +0900

    (Modified)

--- test/test_4.2.cpp
+++ test/test_4.2.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
-//여기에 class 정의
+
 class complex{
 public:
-    int size;
-    int *a;
+    int a;
+    int i;
+
     complex();
-    complex(int size, int newsize);
+    complex(int a,int b);
     ~complex();
     void print();
 };
+
+complex::complex(){
+}
+
+complex::complex(int _a,int _i){
+    a = _a;
+    i = _i;
+
+}
+
+complex::~complex(){
+
+}
+
+void complex::print(){
+    cout << "complex " << a << " " << i << endl;
+}
+
+
 
 int main()
 {
@@ -21,23 +42,26 @@ int main()
     complex* ptr;
 
     //아래에 s크기의 동적 메모리 할당 코드 작성....
-    int *ptr = new int[s];
+    ptr = new complex[s];
     //위에 s크기의 동적 메모리 할당 코드 작성....
 
-    for (int i = 0; i < s; i++) {
-        ptr[i].a = i+s;
-        cout << ptr[i].i << " ";
+    for (int i = 0; i < s; i++){
+        ptr[i].a = i+s; // ptr[0].a = 3 , ptr[1].a = 4 , ptr[i].a = 5
+        ptr[i].i = 7;
+        cout << ptr[i].i << " " ;
     }
     cout << endl;
     complex a(s, s + 10);
     a.print(); // 출력 확인
 
     //아래에 ptr의 모든 요소 print
-   ...
+    for(int i=0;i < s; i++){
+        ptr[i].print();
+    }
     //위에 ptr의 모든 요소 print
 
     //아래에 메모리 반환 코드 작성
-  ...
+    delete [] ptr;
     //위에 메모리 반환 코드 작성
 
     return 0;
