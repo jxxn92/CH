420f7682360c084babbbf371f5ca854c4c3d6622
jxxn92 <futjxxn@naver.com>
2023-05-03 01:34:47 +0900

    (Modified)

--- self_study/operator.cpp
+++ self_study/operator.cpp
@@ -15,21 +15,31 @@ public:
         ++y;
         cout << x << " " << y << endl;
     }
-    Point operator++()
+    Point operator++();
+    Point operator--(int);
+};
+Point Point::operator++() //안에 int 라고 적혀있으면
+                        // 후치임을 알려주는 표식임.
+                        // 반드시 int 라고만 적어야함.
     {
         ++x;
         ++y;
         return *this;
     }
-};
-
+Point Point::operator--(int)                     
+    {
+        x--;
+        y--;
+        return *this;
+    }
 int main(void){
     Point p1(3,4);
-    p1.upPrint();
     ++p1;
-
     cout << p1.getX() << ",";
     cout << p1.getY();
-
+    cout << endl;
+    p1--;
+    cout << p1.getX() << ",";
+    cout << p1.getY();
     return 0;
 }
