c9ee9baf760b197680a94904410cee3945f97404
jxxn92 <futjxxn@naver.com>
2023-05-10 15:43:19 +0900

    (Chapter 9 Assignment Ver 1)

--- OJ/9assignmnet.cpp
+++ OJ/9assignmnet.cpp
@@ -15,19 +15,12 @@ public:
 };
 
 int UI::printGetInt(string msg){
-    cout << msg;
-    int num;
-    cin >> num;
-    return num;
 }
 
 void UI::println(string msg){
-    cout << msg;
-    cout << endl;
 }
 
 void UI::print(string msg){
-    cout << msg;
 }
 
 int UI::getMainMenu(){
@@ -92,6 +85,77 @@ public:
     void move(int width, int height) override;
 };
 
+Line::Line(const Point &p1 , const Point& p2){
+    this -> p1 = p1;
+    this -> p2 = p2;
+}
 void Line::draw(){
     cout << "Line" << endl;
 }
+void Line::draw(){
+    UI::println("Line " + p1.toString() + " " + p2.toString());
+}
+
+void Line::move(int width, int height){
+    p1.move(width,height);
+    p2.move(width,height);
+}
+
+class Rect : public Shape{
+    Point p1;
+    Point p2;
+protected:
+    virtual void draw();
+public:
+    Rect(const Point& p1, const Point& p2);
+    void move(int width, int height) override;
+};
+
+void Rect::draw(){
+    cout << "Rectangle" << endl;
+}
+
+Rect::Rect(const Point& p1, const Point& p2){
+    this -> p1 = p1;
+    this -> p2 = p2;
+}
+
+void Rect::draw(){
+    UI::println("Rectangler " + p1.toString() + " " + p2.toString());
+}
+
+void Rect::move(int width,int height){
+    p1.move(width,height);
+    p2.move(width,height);
+}
+
+class Circle : public Shape {
+    Point center;
+    int radius;
+protected:
+    virtual void draw();
+public:
+    Circle(int radius, const Point& er);
+    void move(int width, int height) override;
+};
+
+Circle::Circle(int radius,const Point& center){
+    this -> radius = radius;
+    this -> center = center;
+}
+
+void Circle::draw(){
+    UI::println("Circle " + to_string(radius) + " " + center.toString());
+}
+
+void Circle::move(int width,int height){
+    center.move(width,height);
+}
+
+class Factory {
+public:
+    enum { LINE = 0, CIRCLE = 1, RECT = 2};
+    static Shape *create();
+    static Shape *create(int shapeType);
+    static int getSize();
+};
